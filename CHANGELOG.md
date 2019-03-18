2683d86ce202571ba514fc13ce0ffc887a5353aa
André Roberge <andre.roberge@gmail.com>
2019-03-18 17:49:13 -0300

    bumped version

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+# Changelog
+
+## Version 0.0.4
+
+First recorded change log.
+
+  - Console works
+  - Diff works
+  - Included dialects: pyupper, pyen, pyfr, pyes
+  - Included languages: en
+  - Custom exceptions implemented
+  - Docs in good enough shape to have been published
+  - Decent test coverage using pytest
+  - etc.
--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 # pylint: skip-file
+import pathlib
 from setuptools import setup, find_packages
-from distutils.util import convert_path
 
-with open("README.md") as f:
-    long_description_ = f.read()
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
 
 with open("avantpy/version.py") as f:
     version_ = f.read().split("=")[-1].strip()[1:-1]
@@ -12,18 +12,19 @@ setup(
     name="avantpy",
     version=version_,
     description="Python with training wheels: executable pseudocode in any language.",
-    long_description=long_description_,
+    long_description=README,
+    long_description_content_type="text/markdown",
     classifiers=[
         "Development Status :: 1 - Planning",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
         "Intended Audience :: Education",
         "Topic :: Education",
     ],
     url="https://github.com/aroberge/avantpy",
     author="André Roberge",
     author_email="Andre.Roberge@gmail.com",
-    license="MIT",
     packages=find_packages(exclude=["dist", "build", "tools"]),
     zip_safe=False,
 )
