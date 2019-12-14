4d5fff618bbc09d04111738888bbeaaebad2d989
André Roberge <andre.roberge@gmail.com>
2019-12-14 13:51:50 -0400

    updating docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,6 +18,7 @@ import sys
 sys.path.insert(0, os.path.abspath("../../"))
 
 from avantpy import version as v
+
 # Sphinx: The short X.Y version
 # This project: we just use one number
 version = v.__version__
@@ -74,18 +75,22 @@ pygments_style = "sphinx"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinxdoc"
+html_theme = "sphinx_rtd_theme"  # installed separately
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#
-# html_theme_options = {}
+html_theme_options = {
+    "prev_next_buttons_location": "both",
+    "style_external_links": True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+html_css_files = ["custom.css"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
